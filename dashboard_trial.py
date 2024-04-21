cfcfb80f59b6ba3bb7f9ea37e888825b3c8439b4
TaufikNH01 <135451899+TaufikNH01@users.noreply.github.com>
2024-04-21 09:03:46 +0200

    Update dashboard_trial.py

--- dashboard_trial.py
+++ dashboard_trial.py
@@ -16,7 +16,7 @@ import geopandas as gpd
 st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
-    theme={base: "light"}
+    theme={"base": "light"}
 )
 
 # === COMPONENT 1 ===
